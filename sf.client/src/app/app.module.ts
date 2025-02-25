305365292fb793ab0033882ec3ffb1e154eb8444
Amoola <hyky69xu@ads.uni-magdeburg.de>
2025-02-25 10:26:44 +0100

    delete all unused components

--- sf.client/src/app/app.module.ts
+++ sf.client/src/app/app.module.ts
@@ -9,36 +9,15 @@ import {MatInputModule} from '@angular/material/input';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { MatListModule } from '@angular/material/list';
-import {ClassListComponent} from "./sf/class/list/class-list.component";
 import {CacheService} from "./services/cache.service";
 import {ApiService} from "./services/api.service";
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {ClassDeleteModalComponent} from './sf/class/delete-modal/class-delete-modal.component';
 import {MatDialogModule} from '@angular/material/dialog';
-import {ClassEditAddModalComponent} from './sf/class/edit-add-modal/class-edit-add-modal.component';
-import {ErrorBannerComponent} from './common/banners/error-banner/error-banner.component';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {ClassDetailComponent} from "./sf/class/detail/class-detail.component";
-import {StudentListComponent} from './sf/student/student-list/student-list.component';
-import {DetailBannerComponent} from './sf/tutor/detail-banner/detail-banner.component';
-import {SchoolListComponent} from './sf/school/list/school-list.component';
-import {SchoolCardComponent} from './sf/school/school-card/school-card.component';
-import {HomeComponent} from './common/home/home.component';
-import {CounterComponent} from "./common/counters/bas/counter/counter.component";
-import {SchoolCounterComponent} from "./common/counters/school-counter/school-counter.component";
-import {ClassCounterComponent} from "./common/counters/class-counter/class-counter.component";
-import {TutorCounterComponent} from "./common/counters/tutor-counter/tutor-counter.component";
-import {StudentCounterComponent} from "./common/counters/student-counter/student-counter.component";
-import {UserCounterComponent} from "./common/counters/user-counter/user-counter.component";
-import {Placement} from "./enums/placement.enum";
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatCard, MatCardActions, MatCardContent, MatCardTitle} from "@angular/material/card";
-import { SelectUserComponent } from './sf/user/select-user/select-user.component';
-import { CreateUserComponent } from './sf/user/create-user/create-user.component';
-import { SelectSchoolComponent } from './sf/school/select-school/select-school.component';
-import { CreateSchoolComponent } from './sf/school/create-school/create-school.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeCoverComponent } from './main-layout/home-cover/home-cover.component';
 import { AboutSectionComponent } from './main-layout/about-section/about-section.component';
@@ -64,26 +43,7 @@ import { TopClassComponent } from './main-layout/top-class/top-class.component';
 
     declarations: [
         AppComponent,
-        ClassListComponent,
-        ClassDeleteModalComponent,
-        ClassEditAddModalComponent,
-        ErrorBannerComponent,
-        ClassDetailComponent,
-        StudentListComponent,
-        DetailBannerComponent,
-        SchoolCardComponent,
-        SchoolListComponent,
-        HomeComponent,
-        CounterComponent,
-        SchoolCounterComponent,
-        ClassCounterComponent,
-        TutorCounterComponent,
-        StudentCounterComponent,
-        UserCounterComponent,
-        SelectUserComponent,
-        CreateUserComponent,
-        SelectSchoolComponent,
-        CreateSchoolComponent,
+ 
         NavBarComponent,
         HomeCoverComponent,
         AboutSectionComponent,
@@ -144,7 +104,7 @@ import { TopClassComponent } from './main-layout/top-class/top-class.component';
         provideAnimationsAsync()
     ],
     exports: [
-        SchoolCardComponent,
+      //  SchoolCardComponent,
         
     ]
 })
--- sf.client/src/app/enums/placement.enum.ts
+++
@@ -1,8 +0,0 @@
-﻿export enum Placement {
-    TopLeft = 'top-left',
-    TopRight = 'top-right',
-    BottomLeft = 'bottom-left',
-    BottomRight = 'bottom-right',
-    InlineLeft = 'inline-left',
-    InlineRight = 'inline-right'
-}
